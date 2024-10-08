6f3ec875f74258072414446a9a6eff74a5d7f149
Clement <clementmaganza@gmail.com>
2024-10-08 11:54:01 +0200

    commencement de api meteo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-let resultatDepartement = document.getElementById("resultatDepartement")
+let resultatCodePostal = document.getElementById("resultatCodePostal")
+let resultatmeteoLatitude = document.getElementById("resultatmeteoLatitude")
+let resultatmeteoLontitude = document.getElementById("resultatmeteoLontitude")
 var zoneCodePostal = document.getElementById("zoneCodePostal");
 var str = ""
 var verifCaractere
+
+
+const token = "7098b091691f53b4ba9f102d5c8a5018c423a36c5eb9e5d061bcfc050d3b0e8b"
+let codeInsee
+
 console.log(str);
 zoneCodePostal.addEventListener("input", recherche);
 
@@ -15,9 +22,11 @@ function recherche(valeur){
         str = verifCaractere
         if (str.length == 5){
             afficheVille()
+            getInsee()
+            afficheMeteo()
         }
         else{
-            resultatDepartement.innerText = "resultat :"
+            resultatCodePostal.innerText = "resultat :"
         }
     }
     console.log(str.length)
@@ -40,9 +49,41 @@ function afficheVille(){
     })
     .then(data => {
         console.log(data);
-        resultatDepartement.innerText = resultatDepartement.textContent + ' ' + data[0].nom;
+        resultatCodePostal.innerText = resultatCodePostal.textContent + ' ' + data[0].nom;
     })
     .catch(error => {
         alert("Attention le code postale n'existe pas")
+    });
+}
+
+function getInsee(){
+    fetch("https://api.meteo-concept.com/api/location/cities?token=" + token + "&search=" + parseInt(str))
+    .then(reponse => {
+    if(!reponse.ok){
+        throw new Error("Network response was not ok");
+    }
+        return reponse.json();
+    })
+    .then(data => {
+        codeInsee = data.cities[0].insee
+    })
+    .catch(error => {
+        alert("Attention insee bug")
+    });
+}
+
+function afficheMeteo(){
+    fetch("https://api.meteo-concept.com/api/forecast-day/1?token=" + token + "&insee=" + parseInt(codeInsee))
+    .then(reponse => {
+    if(!reponse.ok){
+        throw new Error("Network response was not ok");
+    }
+        return reponse.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        alert("Attention meteo bug")
     });
 }
